aa4cdb896e1300346daeda9d39fe65484edefc07
frimpsss <afrimpong031@gmail.com>
2023-11-19 18:34:43 +0000

    setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+function App() {
+
+  return (
+    <>
+      <div className="w-full h-full grid place-items-center">
+          <p>Hello world</p>
+      </div>
+    </>
+  )
+}
+
+export default App
