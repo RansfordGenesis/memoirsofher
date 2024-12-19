3a0e045cd15c356719c783fb48b0d41d1bd4732b
frimpsss <afrimpong031@gmail.com>
2024-12-19 21:24:15 +0000

    finish home page

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,30 +1,14 @@
 import { createBrowserRouter } from "react-router-dom";
-import AuthLayout from "../layouts/auth/Auth.layout";
-import Signin from "../pages/auth/Signin";
-import Signup from "../pages/auth/Signup";
-import App from "../App";
-import { default as MainApp } from "../pages/app/App";
+import HomePage from "../pages/Home";
+
 export const router = createBrowserRouter([
   {
-    path: "/auth/",
-    element: <AuthLayout />,
-    children: [
-      {
-        path: "login",
-        element: <Signin />,
-      },
-      {
-        path: "register",
-        element: <Signup />,
-      },
-    ],
+    path: "",
+    element: <HomePage />,
   },
+
   {
-    path: "/",
-    element: <App />,
-  },
-  {
-    path: "/dashboard",
-    element: <MainApp />,
+    path: "*",
+    element: <HomePage />,
   },
 ]);
