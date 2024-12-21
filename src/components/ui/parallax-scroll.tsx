40151b21f01ea741d8ae8917edd40e7d058def95
Papa Yaw Agyeman-Gyekye. <96929032+PYAG1@users.noreply.github.com>
2024-12-21 12:14:56 +0000

    parallax effect

---
+++ src/components/ui/parallax-scroll.tsx
@@ -0,0 +1,59 @@
+"use client";
+import { useScroll, useTransform } from "framer-motion";
+import { useRef } from "react";
+import { motion } from "framer-motion";
+
+import { cn } from "@/lib/utils";
+import MemoryCard from "../memory-card";
+
+interface CardData {
+  imgUrl: string;
+  tags: string[];
+  title: string;
+  message: string;
+  name?: string;
+}
+
+export const ParallaxScroll = ({
+  images,
+  cards,
+  className,
+}: {
+  images: string[];
+  cards: CardData[][];
+  className?: string;
+}) => {
+  const gridRef = useRef<any>(null);
+  const { scrollYProgress } = useScroll();
+
+  console.log("hei", cards);
+
+  const translate = cards.map((_, index) =>
+    useTransform(scrollYProgress, [0, 1], [0, index % 2 === 0 ? -200 : 200])
+  );
+
+  return (
+    <div className={cn("h-full items-start w-full", className)} ref={gridRef}>
+      <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 items-start w-full mx-auto gap-10  ">
+        {cards.map((cardData, idx) => (
+          <div className="grid gap-10" key={`grid-${idx}`}>
+            {cardData.map((item, idx2) => (
+              <motion.div
+                style={{ y: translate[idx] }}
+                key={`card-${idx}-${idx2}`}
+              >
+                <MemoryCard
+                  tags={item.tags}
+                  title={item.title}
+                  message={item.message}
+                  imageUrl={item.imgUrl}
+                  author={item.name || "Anon"}
+                />
+              </motion.div>
+            ))}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
--- src/pages/Memories.tsx
+++ src/pages/Memories.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 import { splitArrayRoundRobin } from "@/utils";
 // import { useLocation, useNavigate } from "react-router-dom";
 import NavBar from "@/components/shared/nav";
+import { ParallaxScroll } from "@/components/ui/parallax-scroll";
 
 const MemoriesPage = () => {
   const [data, setData] = useState<any[]>([]);
@@ -47,6 +48,7 @@ const MemoriesPage = () => {
   //     title: "Share a memory",
   //   },
   // ];
+
   return (
     <div className=" px-4 lg:mx-auto relative  pb-10 ">
       <NavBar
@@ -57,9 +59,9 @@ const MemoriesPage = () => {
         Memory Wall
       </h4>
 
-      <div className="grid lg:grid-cols-5 gap-4 md:grid-cols-3">
+     
         {loading ? (
-          <>
+         <div className="grid lg:grid-cols-5 gap-4 md:grid-cols-3">
             {splitArrayRoundRobin(
               Array.from(
                 { length: 10 },
@@ -84,31 +86,40 @@ const MemoriesPage = () => {
                 </div>
               );
             })}
-          </>
+          </div>
         ) : (
           <>
-            {data?.map((col, inx) => {
-              return (
-                <div className="flex flex-col h-fit gap-4" key={inx}>
-                  {col.map((cardData: any) => {
-                    return (
-                      <MemoryCard
-                        key={cardData?.imgUrl}
-                        tags={cardData?.tags}
-                        title={cardData?.title}
-                        message={cardData?.message}
-                        imageUrl={cardData?.imgUrl}
-                        author={cardData?.name || "Anon"}
-                      />
-                    );
-                  })}
-                </div>
-              );
-            })}
+             <ParallaxScroll className=" w-full h-full" images={[
+              "https://images.unsplash.com/photo-1554080353-a576cf803bda?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3387&q=80",
+              "https://images.unsplash.com/photo-1505144808419-1957a94ca61e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3070&q=80",
+              "https://images.unsplash.com/photo-1470252649378-9c29740c9fa8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3540&q=80",
+              "https://images.unsplash.com/photo-1554080353-a576cf803bda?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3387&q=80",
+              "https://images.unsplash.com/photo-1505144808419-1957a94ca61e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3070&q=80",
+              "https://images.unsplash.com/photo-1470252649378-9c29740c9fa8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3540&q=80",
+              "https://images.unsplash.com/photo-1682686581854-5e71f58e7e3f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3540&q=80",
+              "https://images.unsplash.com/photo-1510784722466-f2aa9c52fff6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3540&q=80",
+              "https://images.unsplash.com/photo-1505765050516-f72dcac9c60e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3540&q=80",
+              "https://images.unsplash.com/photo-1439853949127-fa647821eba0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2640&q=80",
+              "https://images.unsplash.com/photo-1554080353-a576cf803bda?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3387&q=80",
+              "https://images.unsplash.com/photo-1505144808419-1957a94ca61e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3070&q=80",
+              "https://images.unsplash.com/photo-1470252649378-9c29740c9fa8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3540&q=80",
+              "https://images.unsplash.com/photo-1554080353-a576cf803bda?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3387&q=80",
+              "https://images.unsplash.com/photo-1505144808419-1957a94ca61e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3070&q=80",
+              "https://images.unsplash.com/photo-1554080353-a576cf803bda?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3387&q=80",
+              "https://images.unsplash.com/photo-1505144808419-1957a94ca61e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3070&q=80",
+              "https://images.unsplash.com/photo-1470252649378-9c29740c9fa8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3540&q=80",
+              "https://images.unsplash.com/photo-1554080353-a576cf803bda?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3387&q=80",
+              "https://images.unsplash.com/photo-1505144808419-1957a94ca61e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3070&q=80",
+              "https://images.unsplash.com/photo-1554080353-a576cf803bda?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3387&q=80",
+              "https://images.unsplash.com/photo-1505144808419-1957a94ca61e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3070&q=80",
+              "https://images.unsplash.com/photo-1470252649378-9c29740c9fa8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3540&q=80",
+              "https://images.unsplash.com/photo-1554080353-a576cf803bda?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3387&q=80",
+              "https://images.unsplash.com/photo-1505144808419-1957a94ca61e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3070&q=80",
+            ]} cards={data}  />;
           </>
         )}
       </div>
-    </div>
+
   );
 };
 
