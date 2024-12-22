fcf100a803098b067af7cb9ea936ec99fd0fa6e2
Jessy Baki <bakijessy@gmail.com>
2024-12-22 12:09:52 +0000

    feat: Replace bg img with video for enhanced visual experience on homepage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -45,14 +45,19 @@ const HomePage = () => {
 	return (
 		<div className="h-screen w-screen bg-black relative overflow-clip grid place-content-center">
 			<NavBar />
-			{/* <div className="w-full h-full absolute left-0 right-0 top-0 grid place-content-center z-0">
+			<div className="w-full h-full absolute left-0 right-0 top-0 grid place-content-center z-0">
 				<div className="w-full h-full bg-black/[0.6] absolute left-0 right-0 top-0 backdrop-blur-[2px]"></div>
-				<img
-					src="/images/jojo.jpeg"
-					alt="jojo"
-					className="w-full h-full object-contain"
-				/>
-			</div> */}
+				<video
+					autoPlay
+					loop
+					muted
+					playsInline
+					className="w-full h-full object-cover absolute top-0 left-0"
+				>
+					<source src="/video/jojo.mp4" type="video/mp4" />
+					Your browser does not support the video tag.
+				</video>
+			</div>
 
 			<div className="text-white z-50 flex items-center justify-center flex-col px-4 lg:px-2 lg:gap-8 gap-6">
 				<p className="font-cursive font-extralight text-[2.4rem] lg:text-[5.5rem] text-center">
