ee4177bee9959796f532ace71248e9cee3987d5b
Jessy Baki <bakijessy@gmail.com>
2024-12-22 05:05:53 +0000

    refactor: Simplify NavBar props and improve layout structure

--- src/layout/parallax-page.layout.tsx
+++ src/layout/parallax-page.layout.tsx
@@ -2,23 +2,20 @@ import NavBar from "@/components/shared/nav";
 import React from "react";
 
 interface Props {
-  title: string;
-  children: React.ReactNode;
+	title: string;
+	children: React.ReactNode;
 }
 const SharedLayout = ({ title, children }: Props) => {
-  return (
-    <div className=" px-4 lg:mx-auto relative  pb-10 ">
-      <NavBar
-        linkClass="hover:border-b-black text-black border-transparent"
-        undelinecolor="border-b-black"
-      />
-      <h4 className="text-center font-cursive lg:text-[4.8rem] text-[2.3rem] mb-4 pt-[4.5rem] ">
-        {title}
-      </h4>
+	return (
+		<div className=" px-4 lg:mx-auto relative  pb-10 ">
+			<NavBar linkClass="text-black" underlineColor="bg-black" />
+			<h4 className="text-center font-cursive lg:text-[4.8rem] text-[2.3rem] mb-4 pt-[4.5rem] ">
+				{title}
+			</h4>
 
-      {children}
-    </div>
-  );
+			{children}
+		</div>
+	);
 };
 
 export default SharedLayout;
